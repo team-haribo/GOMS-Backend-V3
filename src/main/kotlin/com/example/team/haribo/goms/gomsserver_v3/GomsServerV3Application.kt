a558ac174ab59007c01eca983408078726c66952
hej090224 <fc49854985@gmail.com>
2026-01-31 17:54:55 +0900

    init

---
+++ src/main/kotlin/com/example/team/haribo/goms/gomsserver_v3/GomsServerV3Application.kt
@@ -0,0 +1,11 @@
+package com.example.team.haribo.goms.gomsserver_v3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GomsServerV3Application
+
+fun main(args: Array<String>) {
+    runApplication<GomsServerV3Application>(*args)
+}
---
+++ src/test/kotlin/com/example/team/haribo/goms/gomsserver_v3/GomsServerV3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.team.haribo.goms.gomsserver_v3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GomsServerV3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
