86d5a389d26968f6dcdcac853ada74434626be2c
hej090224 <fc49854985@gmail.com>
2026-02-12 01:20:57 +0900

    feat: enum 정의

---
+++ src/main/kotlin/com/example/team/haribo/goms/domain/common/enums/Purpose.kt
@@ -0,0 +1,6 @@
+package com.example.team.haribo.goms.domain.common.enums
+
+enum class Purpose {
+    SIGNUP,
+    PASSWORD_CHANGE
+}
---
+++ src/main/kotlin/com/example/team/haribo/goms/domain/common/enums/Role.kt
@@ -0,0 +1,6 @@
+package com.example.team.haribo.goms.domain.common.enums
+
+enum class Role {
+    ROLE_STUDENT,
+    ROLE_STUDENT_COUNCIL
+}
