da48f2185aa70788340c99a69d7f68b684f30a0d
hej090224 <fc49854985@gmail.com>
2026-02-16 01:58:46 +0900

    feat: 사용자 조회용 응답 DTO 추가

---
+++ src/main/kotlin/com/example/team/haribo/goms/domain/studentcouncil/dto/response/StudentsListResponse.kt
@@ -0,0 +1,8 @@
+package com.example.team.haribo.goms.domain.studentcouncil.dto.response
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class StudentsListResponse(
+    @JsonProperty("students")
+    val students: List<StudentResponse>
+)
