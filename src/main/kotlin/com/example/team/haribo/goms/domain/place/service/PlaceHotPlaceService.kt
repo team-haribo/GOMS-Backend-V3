62454cf53bb0abad31f7cd3f12543086c24db131
hej090224 <fc49854985@gmail.com>
2026-02-22 19:51:19 +0900

    feat: 최근 N일 기준 인기 장소 조회 로직 추가

--- src/main/kotlin/com/example/team/haribo/goms/domain/place/service/PlaceHotPlaceService.kt
+++ src/main/kotlin/com/example/team/haribo/goms/domain/place/service/PlaceHotPlaceService.kt
@@ -3,5 +3,5 @@ package com.example.team.haribo.goms.domain.place.service
 import com.example.team.haribo.goms.domain.place.dto.response.PlacesResponse
 
 interface PlaceHotPlaceService {
-    fun getHotPlaces(): PlacesResponse
+    fun getHotPlaces(days: Long?): PlacesResponse
 }
