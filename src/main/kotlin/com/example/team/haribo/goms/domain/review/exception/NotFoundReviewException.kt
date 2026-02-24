2b7a3db8d848555056458f0ebfb3635203a6ce4c
hej090224 <fc49854985@gmail.com>
2026-02-24 17:21:42 +0900

    feat: 후기 도메인 예외 및 에러코드 추가

---
+++ src/main/kotlin/com/example/team/haribo/goms/domain/review/exception/NotFoundReviewException.kt
@@ -0,0 +1,6 @@
+package com.example.team.haribo.goms.domain.review.exception
+
+import com.example.team.haribo.goms.global.exception.ErrorCode
+import com.example.team.haribo.goms.global.exception.GlobalException
+
+class NotFoundReviewException : GlobalException(ErrorCode.NOT_FOUND_REVIEW)
