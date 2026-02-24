32c3a279847df291ec8014619895077d7f5bd9a5
Kim Tae Eun <snowykte0426@naver.com>
2026-02-24 23:07:26 +0900

    docs: ReviewDeleteServiceImpl 주석 추가

--- src/main/kotlin/com/example/team/haribo/goms/domain/review/service/impl/ReviewDeleteServiceImpl.kt
+++ src/main/kotlin/com/example/team/haribo/goms/domain/review/service/impl/ReviewDeleteServiceImpl.kt
@@ -14,6 +14,16 @@ class ReviewDeleteServiceImpl(
     private val memberUtil: MemberUtil
 ) : ReviewDeleteService {
 
+    /**
+     * 작성자 본인이 직접 삭제하는 경우 물리적으로 제거합니다.
+     *
+     * 신고를 통한 학생회 처리 삭제는 감사 추적(audit trail)을 위해 소프트 딜리트로 처리하며,
+     * 해당 로직은 신고/학생회 도메인의 별도 서비스에서 담당합니다.
+     *
+     * @param reviewId 삭제할 리뷰의 ID
+     * @throws NotFoundReviewException 리뷰가 존재하지 않는 경우
+     * @throws ReviewForbiddenException 본인이 작성한 리뷰가 아닌 경우
+     */
     @Transactional
     override fun delete(reviewId: Long) {
         val memberId = memberUtil.currentMemberId()
