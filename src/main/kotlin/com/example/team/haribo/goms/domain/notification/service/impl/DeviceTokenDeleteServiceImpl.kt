7b88074d0d632c60f8504f7e78b8a28f4ebe5c3a
hej090224 <fc49854985@gmail.com>
2026-03-05 12:26:17 +0900

    refactor: 디바이스 토큰 중복 등록 방지 로직 추가

--- src/main/kotlin/com/example/team/haribo/goms/domain/notification/service/impl/DeviceTokenDeleteServiceImpl.kt
+++ src/main/kotlin/com/example/team/haribo/goms/domain/notification/service/impl/DeviceTokenDeleteServiceImpl.kt
@@ -1,9 +1,6 @@
 package com.example.team.haribo.goms.domain.notification.service.impl
 
-import com.example.team.haribo.goms.domain.notification.dto.request.DeviceTokenDeleteRequest
-import com.example.team.haribo.goms.domain.notification.exception.NotFoundDeviceTokenException
 import com.example.team.haribo.goms.domain.notification.repository.DeviceTokenRepository
-import com.example.team.haribo.goms.domain.notification.service.DeviceTokenDeleteService
 import com.example.team.haribo.goms.global.util.MemberUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,18 +9,11 @@ import org.springframework.transaction.annotation.Transactional
 class DeviceTokenDeleteServiceImpl(
     private val deviceTokenRepository: DeviceTokenRepository,
     private val memberUtil: MemberUtil
-) : DeviceTokenDeleteService {
+) {
 
     @Transactional
-    override fun delete(request: DeviceTokenDeleteRequest) {
-
+    fun delete(deviceId: String) {
         val memberId = memberUtil.currentMemberId()
-
-        val deleted = deviceTokenRepository
-            .deleteByMember_IdAndDeviceId(memberId, request.deviceId)
-
-        if (deleted == 0L) {
-            throw NotFoundDeviceTokenException()
-        }
+        deviceTokenRepository.deleteByMember_IdAndDeviceId(memberId, deviceId)
     }
 }
