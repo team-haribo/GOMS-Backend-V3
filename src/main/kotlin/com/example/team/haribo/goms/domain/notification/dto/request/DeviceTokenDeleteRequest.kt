c9811405f7aabaf20fb94cac5dfc77a3a1ad03ed
hej090224 <fc49854985@gmail.com>
2026-03-07 01:08:22 +0900

    refactor: 불필요한 dto 삭제

--- src/main/kotlin/com/example/team/haribo/goms/domain/notification/dto/request/DeviceTokenDeleteRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.team.haribo.goms.domain.notification.dto.request
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class DeviceTokenDeleteRequest(
-
-    @JsonProperty("device_id")
-    val deviceId: String
-)
