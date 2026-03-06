3183fc289247ef22fdfcc0d0e6d6326a194b9234
hej090224 <fc49854985@gmail.com>
2026-03-07 00:57:55 +0900

    feat: SchedulerConfig 추가

--- src/main/kotlin/com/example/team/haribo/goms/GomsServerV3Application.kt
+++ src/main/kotlin/com/example/team/haribo/goms/GomsServerV3Application.kt
@@ -2,9 +2,7 @@ package com.example.team.haribo.goms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
 @SpringBootApplication
 class GomsServerV3Application
 
