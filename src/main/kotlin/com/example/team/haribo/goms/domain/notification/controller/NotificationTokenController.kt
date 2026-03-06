46ea77564792331ac8e6d34ee3c45d9dc9e943f3
hej090224 <fc49854985@gmail.com>
2026-03-07 00:57:04 +0900

    refactor: 구현체 직접 참조 수정

--- src/main/kotlin/com/example/team/haribo/goms/domain/notification/controller/NotificationTokenController.kt
+++ src/main/kotlin/com/example/team/haribo/goms/domain/notification/controller/NotificationTokenController.kt
@@ -1,26 +1,35 @@
 package com.example.team.haribo.goms.domain.notification.controller
 
 import com.example.team.haribo.goms.domain.notification.dto.request.DeviceTokenRegisterRequest
-import com.example.team.haribo.goms.domain.notification.service.impl.DeviceTokenDeleteServiceImpl
-import com.example.team.haribo.goms.domain.notification.service.impl.DeviceTokenRegisterServiceImpl
+import com.example.team.haribo.goms.domain.notification.service.DeviceTokenDeleteService
+import com.example.team.haribo.goms.domain.notification.service.DeviceTokenRegisterService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v3/notification")
 class NotificationTokenController(
-    private val deviceTokenRegisterService: DeviceTokenRegisterServiceImpl,
-    private val deviceTokenDeleteService: DeviceTokenDeleteServiceImpl
+    private val deviceTokenRegisterService: DeviceTokenRegisterService,
+    private val deviceTokenDeleteService: DeviceTokenDeleteService
 ) {
 
     @PostMapping("/token")
-    fun register(@RequestBody request: DeviceTokenRegisterRequest): ResponseEntity<Void> {
+    fun register(
+        @RequestBody request: DeviceTokenRegisterRequest
+    ): ResponseEntity<Void> {
         deviceTokenRegisterService.register(request)
         return ResponseEntity.ok().build()
     }
 
     @DeleteMapping("/token/{deviceId}")
-    fun delete(@PathVariable deviceId: String): ResponseEntity<Void> {
+    fun delete(
+        @PathVariable deviceId: String
+    ): ResponseEntity<Void> {
         deviceTokenDeleteService.delete(deviceId)
         return ResponseEntity.ok().build()
     }
