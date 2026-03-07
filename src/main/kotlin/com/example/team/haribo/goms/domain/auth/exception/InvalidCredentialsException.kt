64619134297548ed5b4ef068083a3b370830a167
hej090224 <fc49854985@gmail.com>
2026-03-07 13:12:50 +0900

    refactor: 불필요한 예외 제거

--- src/main/kotlin/com/example/team/haribo/goms/domain/auth/exception/InvalidCredentialsException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.team.haribo.goms.domain.auth.exception
-
-import com.example.team.haribo.goms.global.exception.ErrorCode
-import com.example.team.haribo.goms.global.exception.GlobalException
-
-class InvalidCredentialsException : GlobalException(ErrorCode.INVALID_CREDENTIALS)
