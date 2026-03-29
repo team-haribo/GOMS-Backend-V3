dca97a79cdb1d4be1001534b11cd0c93ab26aad9
hej090224 <fc49854985@gmail.com>
2026-03-29 12:27:14 +0900

    refactor: 학생별 응답에서 성별 제거

--- src/main/kotlin/com/example/team/haribo/goms/domain/studentcouncil/dto/response/StudentResponse.kt
+++ src/main/kotlin/com/example/team/haribo/goms/domain/studentcouncil/dto/response/StudentResponse.kt
@@ -1,12 +1,10 @@
 package com.example.team.haribo.goms.domain.studentcouncil.dto.response
 
 import com.example.team.haribo.goms.domain.common.enums.Department
-import com.example.team.haribo.goms.domain.common.enums.Gender
 
 data class StudentResponse(
     val memberId: Long,
     val name: String,
     val grade: Int,
-    val department: Department,
-    val gender: Gender
+    val department: Department
 )
