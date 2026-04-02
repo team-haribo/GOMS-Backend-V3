d4518833b7f56d4f530428fcc5904e09e2bea567
hej090224 <fc49854985@gmail.com>
2026-04-02 20:47:49 +0900

    feat: 상수 클래스 추가

---
+++ src/main/kotlin/com/example/team/haribo/goms/global/log/RequestLogConstants.kt
@@ -0,0 +1,10 @@
+package com.example.team.haribo.goms.global.log
+
+object RequestLogConstants {
+    const val START_TIME = "requestStartTime"
+    const val REQUEST_ID = "requestId"
+    const val FAILURE_REASON = "failureReason"
+    const val EXCEPTION_TYPE = "exceptionType"
+    const val MEMBER_ID = "memberId"
+    const val ROLE = "role"
+}
