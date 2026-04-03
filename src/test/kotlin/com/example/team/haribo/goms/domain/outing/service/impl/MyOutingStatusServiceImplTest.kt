512db25209790929a74d59613efba73dd807f391
hej090224 <fc49854985@gmail.com>
2026-04-03 14:09:03 +0900

    refactor: 테스트 코드 수정

--- src/test/kotlin/com/example/team/haribo/goms/domain/outing/service/impl/MyOutingStatusServiceImplTest.kt
+++ src/test/kotlin/com/example/team/haribo/goms/domain/outing/service/impl/MyOutingStatusServiceImplTest.kt
@@ -1,6 +1,7 @@
 package com.example.team.haribo.goms.domain.outing.service.impl
 
 import com.example.team.haribo.goms.domain.common.enums.Status
+import com.example.team.haribo.goms.domain.late.repository.LateRepository
 import com.example.team.haribo.goms.fixture.MemberFixture
 import com.example.team.haribo.goms.global.util.MemberUtil
 import io.kotest.core.spec.style.DescribeSpec
@@ -11,29 +12,40 @@ import io.mockk.mockk
 class MyOutingStatusServiceImplTest : DescribeSpec({
 
     val memberUtil = mockk<MemberUtil>()
-    val service = MyOutingStatusServiceImpl(memberUtil)
+    val lateRepository = mockk<LateRepository>()
+    val service = MyOutingStatusServiceImpl(memberUtil, lateRepository)
 
     describe("MyOutingStatusService") {
 
         context("Given: COMING 상태 멤버") {
             val member = MemberFixture.student(status = Status.COMING)
             every { memberUtil.currentMember() } returns member
+            every { lateRepository.countByMember_Id(member.id!!) } returns 0L
 
             it("When: 외출 상태 조회 시 Then: COMING 상태를 반환한다") {
                 val response = service.getStatus()
                 response.status shouldBe Status.COMING
                 response.memberId shouldBe member.id!!
                 response.name shouldBe member.name
+                response.grade shouldBe member.grade
+                response.department shouldBe member.department.name
+                response.lateCount shouldBe 0L
             }
         }
 
         context("Given: OUTING 상태 멤버") {
             val member = MemberFixture.outing()
             every { memberUtil.currentMember() } returns member
+            every { lateRepository.countByMember_Id(member.id!!) } returns 2L
 
             it("When: 외출 상태 조회 시 Then: OUTING 상태를 반환한다") {
                 val response = service.getStatus()
                 response.status shouldBe Status.OUTING
+                response.memberId shouldBe member.id!!
+                response.name shouldBe member.name
+                response.grade shouldBe member.grade
+                response.department shouldBe member.department.name
+                response.lateCount shouldBe 2L
             }
         }
     }
