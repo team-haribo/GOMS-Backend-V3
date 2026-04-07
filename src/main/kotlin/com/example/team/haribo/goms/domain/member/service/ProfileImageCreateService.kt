d509a6c30052de88652a2f0d22d4be5084245066
hej090224 <fc49854985@gmail.com>
2026-04-08 08:10:19 +0900

    refactor: 최초등록 서비스 제거

--- src/main/kotlin/com/example/team/haribo/goms/domain/member/service/ProfileImageCreateService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.team.haribo.goms.domain.member.service
-
-import com.example.team.haribo.goms.domain.member.dto.response.ProfileImageResponse
-import org.springframework.web.multipart.MultipartFile
-
-interface ProfileImageCreateService {
-    fun execute(image: MultipartFile): ProfileImageResponse
-}
