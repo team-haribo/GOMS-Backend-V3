fa95dc11228b6e136f5ad2a97442479fb7971205
hej090224 <fc49854985@gmail.com>
2026-04-09 20:40:51 +0900

    refactor: 타임아웃 설정 추가

--- src/main/kotlin/com/example/team/haribo/goms/domain/place/config/KakaoWebClientConfig.kt
+++ src/main/kotlin/com/example/team/haribo/goms/domain/place/config/KakaoWebClientConfig.kt
@@ -1,13 +1,17 @@
 package com.example.team.haribo.goms.domain.place.config
 
 import io.netty.channel.ChannelOption
+import io.netty.handler.ssl.SslHandler
 import io.netty.handler.timeout.ReadTimeoutHandler
 import io.netty.handler.timeout.WriteTimeoutHandler
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.Http11SslContextSpec
 import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.SslProvider
 import java.time.Duration
 import java.util.concurrent.TimeUnit
 
@@ -15,11 +19,22 @@ import java.util.concurrent.TimeUnit
 class KakaoWebClientConfig {
 
     @Bean
+    @Qualifier("kakaoWebClient")
     fun kakaoWebClient(): WebClient {
+        val sslProvider = SslProvider.builder()
+            .sslContext(Http11SslContextSpec.forClient())
+            .handshakeTimeoutMillis(60_000)
+            .closeNotifyFlushTimeoutMillis(10_000)
+            .closeNotifyReadTimeoutMillis(10_000)
+            .handlerConfigurator { sslHandler: SslHandler ->
+                sslHandler.setHandshakeTimeoutMillis(60_000)
+            }
+            .build()
+
         val httpClient = HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 60000)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 60_000)
             .responseTimeout(Duration.ofSeconds(60))
-            .secure()
+            .secure(sslProvider)
             .doOnConnected { connection ->
                 connection
                     .addHandlerLast(ReadTimeoutHandler(60, TimeUnit.SECONDS))
